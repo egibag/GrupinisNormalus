f588c22873a282eac4cebe5ce887f09392f055f4
TautvydasRimas <tautvydasrimas@gmail.com>
2019-05-22 12:13:34 +0300

    Console

---
+++ 20190521GrupinisRobertas/Console.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20190521GrupinisRobertas
+{
+    class Console : IDuombaze
+    {
+        public void DuomenuTipas()
+        {
+            System.Console.WriteLine("Siusti duomenis");
+        }
+
+        public void GautiDuomenis()
+        {
+            System.Console.WriteLine("Gauti duomenis");
+        }
+
+        public void SiustiDuomenis()
+        {
+            System.Console.WriteLine("Consoles langas");
+        }
+    }
+}
