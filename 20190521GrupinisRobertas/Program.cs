41e0b1e2ddc2adae660fc20308226f401186066e
Marius <35076113+MuchName@users.noreply.github.com>
2019-05-22 19:44:22 +0300

    Csv Failas

--- 20190521GrupinisRobertas/Program.cs
+++ 20190521GrupinisRobertas/Program.cs
@@ -20,24 +20,6 @@ namespace _20190521GrupinisRobertas
         void DuomenuTipas();// 
     }
 
-    class CsvFailas : IDuombaze
-    {
-        public void DuomenuTipas()
-        {
-            System.Console.WriteLine(".csv failas");
-        }
-
-        public void GautiDuomenis()
-        {
-            System.Console.WriteLine("Duomenys gaunami");
-        }
-
-        public void SiustiDuomenis()
-        {
-            System.Console.WriteLine("Duomenys siunciami");
-        }
-    }
-
     class Program
     {
         static void Main(string[] args)
