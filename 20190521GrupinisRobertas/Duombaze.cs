ae40861b9b871599bf00277f649c69de0f206cd2
egibag <egibag@gmail.com>
2019-05-23 10:21:20 +0300

    Sukurta klasė: Duombaze

---
+++ 20190521GrupinisRobertas/Duombaze.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20190521GrupinisRobertas
+{
+    class Duombaze : IDuombaze
+    {
+        public void GautiDuomenis()
+        {
+            Console.WriteLine("Duomenys gaunami!");
+            throw new NotImplementedException();
+        }
+
+        public void SiustiDuomenis()
+        {
+            Console.WriteLine("Duomenys siunčiami!");
+            throw new NotImplementedException();
+        }
+
+        public void DuomenuTipas()
+        {
+            Console.WriteLine("Duomenu tipas duomenu baze!");
+            throw new NotImplementedException();
+        }
+    }
+}
